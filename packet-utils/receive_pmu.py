af993cc455b1b18a82e439aea2ef3ee7e3b1bae9
Jack Norris <jacktimothynorris@gmail.com>
2023-11-25 12:58:58 -0600

    sending and parsing pmu udp packets

---
+++ packet-utils/receive_pmu.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+from scapy.all import (
+    TCP,
+    UDP,
+    FieldLenField,
+    FieldListField,
+    IntField,
+    IPOption,
+    ShortField,
+    get_if_list,
+    sniff
+)
+from scapy.layers.inet import _IPOption_HDR
+import struct
+import math
+
+
+def get_if(ifacename):
+    ifs=get_if_list()
+    iface=None
+    for i in get_if_list():
+        if ifacename in i:
+            iface=i
+            break;
+    if not iface:
+        print("Cannot find %s interface" % ifacename)
+        exit(1)
+    return iface
+
+def pmu_packet_parser(data, settings={"pmu_measurement_bytes": 8, "num_phasors": 1, "freq_bytes": 2, "dfreq_bytes": 2}):
+    freq_start_byte = 16 + settings["num_phasors"] * settings["pmu_measurement_bytes"]
+    dfreq_start_byte = freq_start_byte + settings["freq_bytes"]
+    analog_start_byte = dfreq_start_byte + settings["dfreq_bytes"]
+    digital_start_byte = analog_start_byte + 4
+    chk_start_byte = digital_start_byte + 2
+
+    # convert each field to correct data type
+    pmu_packet = {
+        "sync": int.from_bytes(data[0:2], byteorder="big"),
+        "frame_size": int.from_bytes(data[2:4], byteorder="big"),
+        "id_code": int.from_bytes(data[4:6], byteorder="big"),
+        "soc": int.from_bytes(data[6:10], byteorder="big"),
+        "frac_sec": int.from_bytes(data[10:14], byteorder="big"),
+        "stat": int.from_bytes(data[14:16], byteorder="big"),
+        "phasors": parse_phasors(data[16:16 + settings["pmu_measurement_bytes"]], {"num_phasors": settings["num_phasors"], "pmu_measurement_bytes": settings["pmu_measurement_bytes"]}),
+        "freq": data[freq_start_byte:dfreq_start_byte],
+        "dfreq": data[dfreq_start_byte:analog_start_byte],
+        "analog": data[analog_start_byte:digital_start_byte],
+        "digital": data[digital_start_byte:chk_start_byte],
+        "chk": data[chk_start_byte:]
+    }
+
+    return pmu_packet
+
+def handle_pkt(pkt):
+    if UDP in pkt and pkt[UDP].dport == 1234:
+        print("got a packet")
+        print(str(pkt[UDP].payload.load))
+        print(pmu_packet_parser(pkt[UDP].payload.load))
+        #pkt.show2()
+    #    hexdump(pkt)
+        #sys.stdout.flush()
+
+def parse_phasors(phasor_data, settings={"num_phasors": 1, "pmu_measurement_bytes": 8}):
+    phasor = {
+        "magnitude": struct.unpack('>f', phasor_data[0:int(settings["pmu_measurement_bytes"]/2)])[0],
+        "angle": math.degrees(struct.unpack('>f', phasor_data[int(settings["pmu_measurement_bytes"]/2) : settings["pmu_measurement_bytes"]])[0]),
+    }
+    return [phasor]
+
+def main():
+    if len(sys.argv)<2:
+    	print('pass 1 argument: <iface>')
+    iface = get_if(sys.argv[1])
+    print("sniffing on %s" % iface)
+    sys.stdout.flush()
+    sniff(iface = iface,
+          prn = lambda x: handle_pkt(x))
+
+if __name__ == '__main__':
+    main()
---
+++ packet-utils/send_pmu.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import random
+import socket
+import sys
+import socket
+from datetime import datetime
+import math
+import struct
+import pandas as pd
+import sys
+
+from scapy.all import IP, TCP, UDP, Ether, get_if_hwaddr, get_if_list, sendp
+
+
+def get_if(ifacename):
+    ifs=get_if_list()
+    iface=None # "h1-eno1"
+    for i in get_if_list():
+        if ifacename in i:
+            iface=i
+            break
+    if not iface:
+        print("Cannot find %s interface" % ifacename)
+        exit(1)
+    return iface
+
+
+def generate_packet(time, voltage, angle, settings={"pmu_measurement_bytes": 8, "destination_ip": "192.168.0.100", "destination_port": 4712}):
+    # Define the PMU packet as a byte string
+    datetime_str = str(time)[:26]
+    global index
+    try:
+        dt = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S.%f')
+    except ValueError:
+        dt = datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S')
+
+    # 2 byte
+
+    sync = b'\xAA\x01'
+    #sync = index.to_bytes(2, 'big')
+
+
+    # 2 byte, 44 for 32 bit values of PMU, 40 for 16 bit values of PMU
+    # 36 - 8 + 8 * number of PMUs || 36 - 8 + 4 * number PMUs
+    frame_size = b'\x00\x24'
+
+    # 2 byte, 12 for this
+    id_code = b'\x00\x0C'
+
+    # 4 byte
+    soc = int(dt.strftime("%s")).to_bytes(4, 'big')
+    #print(dt.strftime("%s"))
+    # 4 byte
+    frac_sec = dt.microsecond.to_bytes(4, 'big')
+    # 2 byte (no errors)
+    stat = b'\x00\x00'
+
+    # 4 or 8 byte x number of phasors (see doc, 8 is for float)
+    voltage_bytes = struct.pack('>f', voltage)
+    angle_bytes = struct.pack('>f', math.radians(angle))
+    phasors = voltage_bytes + angle_bytes
+
+    # 2 byte, assumed 60
+    freq = b'\x09\xC4'
+
+    # 2 byte
+    dfreq = b'\x00\x00'
+
+    # 4 byte
+    analog = b'\x00\x00\x00\x00'
+
+    # 2 byte
+    digital = b'\x00\x00'
+
+    # 2 byte
+    chk = b'\x00\x00'
+
+    pmu_packet_payload = sync + frame_size + id_code + soc + frac_sec + stat + phasors + freq + dfreq + analog + digital + chk
+
+    return pmu_packet_payload
+
+def main():
+
+    if len(sys.argv)<3:
+        print('pass 2 arguments: <iface> <destination>')
+        exit(1)
+
+    addr = socket.gethostbyname(sys.argv[2])
+    iface = get_if(sys.argv[1])
+
+    print("sending on interface %s to %s" % (iface, str(addr)))
+    pkt =  Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt /IP(dst=addr) / UDP(dport=1234, sport=random.randint(49152,65535)) / generate_packet('2014-01-28 18:00:13.583000000', 69420, 42000)
+    pkt.show2()
+    sendp(pkt, iface=iface, verbose=False)
+
+
+if __name__ == '__main__':
+    main()
